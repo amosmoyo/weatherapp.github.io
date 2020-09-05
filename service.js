f66c3d56c24023be67c99e5c62b53b22d0ab31c2
amosmoyo <amosmoyo5300@gmail.com>
2020-09-05 20:38:32 +0300

    debug index.js

--- service.js
+++ service.js
@@ -1,20 +1,26 @@
-const casheData = [
-  "./", "./index.html", "./src/style.css", "./images/beautiful-young-girl-yellow-raincoat-600w-1276224355.jpg",
-  "./images/weather.png", "./apms/jpg", "./src/index.js"
-]
-self.addEventListener('install', (e) => {
-  console.log('install');
+let casheData = [
+  './index.html',
+  './src/style.css',
+  "./src/index.js",
+  './images/beautiful-young-girl-yellow-raincoat-600w-1276224355.jpg',
+  './images/weather.png',
+  './images/amps.jpg'
+];
+self.addEventListener("install", (e) => {
+  console.log("install");
   e.waitUntil(
-    caches.open('static').then((cache) => {
+    caches.open("static").then((cache) => {
       return cache.addAll(casheData);
     })
-  )
-})
+  );
+});
 
 self.skipWaiting();
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => {
-    return res || fetch(e.request)
-  }))
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
